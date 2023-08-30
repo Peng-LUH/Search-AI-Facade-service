e99d8f8449dc2e97e17a6cdf83e1fc2381ed721b
Peng-LUH <ethaniel.unihannover@gmail.com>
2023-08-30 09:56:55 +0200

    update

---
+++ swagger_client_L3S/swagger_client/api/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from .downstream_api import DownstreamApi
+from .upstream_api import UpstreamApi
---
+++ swagger_client_L3S/swagger_client_2/api/__init__.py
@@ -0,0 +1,8 @@
+# from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from swagger_client_2.api.pet_api import PetApi
+from swagger_client_2.api.store_api import StoreApi
+from swagger_client_2.api.user_api import UserApi
